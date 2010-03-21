e9f5168db4a65b51c43633f6444860f4768103bd
Christopher Walker <cewalker@puntsok.local>
2010-03-20 20:41:26 -0500

    added rspec testing

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,7 @@
 class Person < ActiveRecord::Base
   
-  validates_presence_of :name
+  validates_presence_of :name 
+  validates_length_of   :name, :within => 2..30
   
   belongs_to :supervisor, :class_name => 'Person',
              :foreign_key => 'supervisor_id'
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,32 @@
 require File.dirname(__FILE__) + '/../spec_helper'
-
-describe Person do
+    
+describe Person, "when first created" do
   
+  before :each do
+    @person     = Person.new 
+    @underling  = Person.new :name => "Mr. Low"   , :supervisor_id => 100
+    @underling2 = Person.new :name => "Mz. Low"   , :supervisor_id => 100
+    @bossman    = Person.new :name => "Bill Gates", :id            => 100
+  end 
+       
+  # start of tests:  ################################# 
+  
+  it "should have a name" do
+    @person.name = ''
+    @person.should_not be_valid
+  end 
+  it "show have a name longer than 2 characters" do
+    @person.name = 'a'
+    @person.should_not be_valid
+  end
+  it "should allow association with manager" do 
+    @underling.supervisor = @bossman
+    @underling.supervisor.name.should == 'Bill Gates'
+  end
+  it "should be allowed overload over several underlings" do
+    @bossman.underlings << @underling
+    @bossman.underlings << @underling2
+    @bossman.underlings.size.should == 2
+  end
+
 end
